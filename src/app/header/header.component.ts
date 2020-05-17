2dd5636e8feaa91e693fd507ea5ed7c4afd387f2
unknown <d.kashmensky@gmail.com>
2020-05-17 16:16:45 +0300

    Add header layout and styles + logout button animation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('openCloseLogout', [
+      state('open', style({
+        top: '80px',
+      })),
+      state('close', style({
+        top: '0px',
+      })),
+      transition('close => open', [
+        animate('300ms')
+      ]),
+      transition('open => close', [
+        animate('300ms')
+      ]),
+    ])
+  ]
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HeaderComponent {
+  showLogout = false;
 }
